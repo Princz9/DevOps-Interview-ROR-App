bbac383fe5a494fd4ac3aeb22474225a54e95da9
Princz <rustynation9@gmail.com>
2025-07-21 19:01:17 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,27 +1,55 @@
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "${var.project_name}-ecs-task-exec-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Effect = "Allow",
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
-        }
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-ecs-task-exec-role"
+  }
+}
+
+resource "aws_iam_policy_attachment" "ecs_task_exec_policy" {
+  name       = "ecs-task-exec-policy-attach"
+  roles      = [aws_iam_role.ecs_task_execution.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy" "s3_access" {
+  name = "ecs-s3-access"
+  role = aws_iam_role.ecs_task_execution.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ],
+        Resource = "arn:aws:s3:::${var.project_name}-bucket/*"
       },
+      {
+        Effect = "Allow",
+        Action = "s3:ListBucket",
+        Resource = "arn:aws:s3:::${var.project_name}-bucket"
+      }
     ]
   })
 }
 
-resource "aws_iam_policy_attachment" "ecs_execution" {
-  name       = "ecs_execution"
-  roles      = [aws_iam_role.ecs_task_execution_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "s3_access" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+output "ecs_task_exec_role_arn" {
+  value = aws_iam_role.ecs_task_execution.arn
 }
