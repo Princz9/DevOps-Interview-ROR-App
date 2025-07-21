726cdb9104bbb172c8aa1c82a80bd042583c9aec
Princz <rustynation9@gmail.com>
2025-07-21 19:05:24 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,29 +1,46 @@
-resource "aws_lb" "ecs_alb" {
-  name               = "ror-app-alb"
+resource "aws_lb" "app_alb" {
+  name               = "${var.project_name}-alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = module.vpc.public_subnets
-  security_groups    = [aws_security_group.alb_sg.id]
-}
+  subnets            = var.subnet_ids
+  security_groups    = [var.sg_id]
 
-resource "aws_lb_target_group" "ecs_tg" {
-  name     = "ror-app-tg"
-  port     = 3000
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
-  target_type = "ip"
-  health_check {
-    path = "/"
+  tags = {
+    Name = "${var.project_name}-alb"
   }
 }
 
-resource "aws_lb_listener" "ecs_listener" {
-  load_balancer_arn = aws_lb.ecs_alb.arn
+resource "aws_lb_target_group" "app_tg" {
+  name     = "${var.project_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${var.project_name}-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.ecs_tg.arn
+    target_group_arn = aws_lb_target_group.app_tg.arn
   }
+}
+
+output "alb_dns_name" {
+  value = aws_lb.app_alb.dns_name
 }
