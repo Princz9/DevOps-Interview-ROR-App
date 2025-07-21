f6c43babae57cf76f88fabe159166c12847ba4ba
Princz <rustynation9@gmail.com>
2025-07-21 18:33:34 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" { default = "ap-south-1" }
+variable "db_name"     { default = "rorappdb" }
+variable "db_user"     { default = "roruser" }
+variable "db_password" { default = "RORdb@12345" }
+variable "project_name" { default = "devops-ror" }
