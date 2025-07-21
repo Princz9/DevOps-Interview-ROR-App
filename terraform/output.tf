3becf178a84031fbe4454d22ac59c97c920348c4
Princz <rustynation9@gmail.com>
2025-07-21 21:23:50 +0530

    Create output.tf

---
+++ terraform/output.tf
@@ -0,0 +1,15 @@
+output "alb_dns_name" {
+  value = module.alb.dns_name
+}
+
+output "rds_endpoint" {
+  value = module.rds.db_endpoint
+}
+
+output "s3_bucket_name" {
+  value = module.s3.bucket_name
+}
+
+output "ecs_service_name" {
+  value = module.ecs.service_name
+}
