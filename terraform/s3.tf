d4ef61b5bff573a67e5c493d84fe99d6a7a9700a
Princz <rustynation9@gmail.com>
2025-07-21 19:04:15 +0530

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,4 +1,21 @@
-resource "aws_s3_bucket" "ror_bucket" {
-  bucket = "${var.project_name}-ror-s3"
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "${var.project_name}-bucket"
   force_destroy = true
+
+  tags = {
+    Name = "${var.project_name}-bucket"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "app_bucket" {
+  bucket = aws_s3_bucket.app_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.app_bucket.bucket
 }
