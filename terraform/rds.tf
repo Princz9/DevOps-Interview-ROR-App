a7d536d89ac3c6eda41dfa64e0aff77b3b1e1de0
Princz <rustynation9@gmail.com>
2025-07-21 18:31:50 +0530

    Create rds.tf

---
+++ terraform/rds.tf
@@ -0,0 +1,18 @@
+resource "aws_db_instance" "postgres" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "15.2"
+  instance_class       = "db.t3.micro"
+  name                 = var.db_name
+  username             = var.db_user
+  password             = var.db_password
+  skip_final_snapshot  = true
+  publicly_accessible  = false
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet.name
+}
+
+resource "aws_db_subnet_group" "rds_subnet" {
+  name       = "rds-subnet"
+  subnet_ids = module.vpc.private_subnets
+}
