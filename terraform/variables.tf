b3a837b9ccdd2ca3bfb4a218dcec76b23d6906fc
Princz <rustynation9@gmail.com>
2025-07-22 12:44:17 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,14 @@
 variable "project_name" {}
-variable "subnet_ids" { type = list(string) }
+variable "image_url" {}
+variable "subnet_ids" {
+  type = list(string)
+}
 variable "sg_id" {}
-variable "vpc_id" {}
+variable "region" {}
+variable "execution_role_arn" {}
+variable "db_host" {}
 variable "db_name" {}
 variable "db_username" {}
 variable "db_password" {}
-variable "image_url" {}
-variable "region" {}
+variable "bucket_name" {}
+variable "target_group_arn" {}
