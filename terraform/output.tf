15545ed7877e9f1d63dd195e51809793ae192c55
Princz <rustynation9@gmail.com>
2025-07-22 14:48:28 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -13,4 +13,11 @@ output "s3_bucket_name" {
 output "ecs_service_name" {
   value = module.ecs.service_name
 }
+output "app_tg_arn" {
+  value = aws_lb_target_group.app.arn
+}
+
+output "dns_name" {
+  value = aws_lb.main.dns_name
+}
 
